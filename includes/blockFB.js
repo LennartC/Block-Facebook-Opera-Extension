7b4c25c92b2079c997f0adaad4e63a6bd9c42233
Lennart Coopmans <coopmansl@gmail.com>
2011-03-12 23:34:25 +0100

    removing debug info

--- includes/blockFB.js
+++ includes/blockFB.js
@@ -10,19 +10,12 @@
 	};
 
 	if (!isFacebookDomain(window.location.hostname)) {
-	  console.log('blocking from: '+window.location.hostname);
 	  window.opera.addEventListener('BeforeScript', function (e) {
 		var hostname = e.element.src.split(/\/+/g)[1];
 		if (hostname && isFacebookDomain(hostname)) {
-		
 			e.preventDefault();
-			console.log("blocking "+hostname);
-			console.log(e);
 		}
 	  }, true);
 
-	} else {
-		console.log('not blocking from: '+window.location.hostname);
 	}
-
 }());
